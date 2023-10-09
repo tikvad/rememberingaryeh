a1ae9d0018b2de70078fe83b2c306a24e43b3292
moriagro <moriagrohar@carmel6000.amitnet.org>
2023-10-09 15:52:39 +0300

    Adding connection to the database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { Post } from './post/post.entity';
 
 @Module({
-  imports: [PostModule],
+  imports: [TypeOrmModule.forRoot({
+    // Everyone update this according to their own database 
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'MySql97531!',
+    database: 'ariyeh_memorial',
+    entities: [Post],
+    synchronize: true,
+  }),
+    PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
