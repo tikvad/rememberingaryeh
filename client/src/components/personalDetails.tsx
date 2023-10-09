264ab1bb256e9e218f80ec38d6dbe1ed5ce19e55
nshakedsl <shakedsolomon@gmail.com>
2023-10-10 01:07:16 +0300

    finish details

--- client/src/components/personalDetails.tsx
+++ client/src/components/personalDetails.tsx
@@ -1,9 +1,26 @@
 import Title from "./Title"
+import "../styles/details.scss";
+
 
 export default function PersonalDetails() {
+    const text = `אריה צירינג ז״ל, בן מארק ודבי, אח גדול לאלי,יונתן וטל. 
+בן 27 בנופלו,
+בוגר מכינת יונתן וישיבת הגלבוע,
+התגייס למסלול השייטת ומשם החליט לעבור לעוקץ. היה לוחם נערץ ביחידת עוקץ ומשם יצא קצונה ופיקד על עשרות חיילים. 
+בתפקידו האחרון שירת כמ״פ תקיפה.
+אריה היה ידוע בענווה ובצניעות שלו, בנתינה שלו, באהבה שלו לארץ ובערכים הרבים שהובילו אותו, היה אח וחבר אהוב. 
+ת.נ.צ.ב.ה`
     return (
-    <div>
-        <Title text="אריה צירינג" />
-    </div>
+        <div className="bigContainer">
+            <Title text="אריה צירינג" />
+            <div className="containerM">
+                <div className="container" >
+                    <img className="image" src="ariyeh.jpg" />
+                    <div className="detailsText">
+                        {text}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
