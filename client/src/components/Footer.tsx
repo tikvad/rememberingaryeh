df1a4d2dae569c55b1ace0f4690d4f27406363a4
tikva <tikvaduker@gmail.com>
2023-10-10 06:21:37 +0300

    fixed minor ts issue

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-const Footer: React.FC<{ text: string }> = () => {
+const Footer: React.FC<{ text?: string }> = () => {
     return <div className="footer">
        Candle icons created by Freepik - Flaticon
     </div>
